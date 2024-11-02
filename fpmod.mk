384d23e1d498f3f9a6424e018b4f54c0514a74c8
Sapan Gajjar <sapangajjar101105@gmail.com>
2024-11-02 13:40:05 +0530

    hanoip: Update paths '**/vendor/lib/' to '**/lib/'
    Signed-off-by: Sapan Gajjar <sapangajjar101105@gmail.com>

--- fpmod.mk
+++ fpmod.mk
@@ -1,6 +1,6 @@
 PRODUCT_COPY_FILES += \
-    vendor/motorola/hanoip-modules/vendor/lib/modules/fpc1020_mmi.ko:$(TARGET_COPY_OUT_VENDOR)/lib/modules/fpc1020_mmi.ko \
-    vendor/motorola/hanoip-modules/vendor/lib/modules/fpsensor_spi_tee.ko:$(TARGET_COPY_OUT_VENDOR)/lib/modules/fpsensor_spi_tee.ko \
-    vendor/motorola/hanoip-modules/vendor/lib/modules/modules.alias:$(TARGET_COPY_OUT_VENDOR)/lib/modules/modules.alias \
-    vendor/motorola/hanoip-modules/vendor/lib/modules/modules.dep:$(TARGET_COPY_OUT_VENDOR)/lib/modules/modules.dep \
-    vendor/motorola/hanoip-modules/vendor/lib/modules/modules.softdep:$(TARGET_COPY_OUT_VENDOR)/lib/modules/modules.softdep 
+    vendor/motorola/hanoip-modules/lib/modules/fpc1020_mmi.ko:$(TARGET_COPY_OUT_VENDOR)/lib/modules/fpc1020_mmi.ko \
+    vendor/motorola/hanoip-modules/lib/modules/fpsensor_spi_tee.ko:$(TARGET_COPY_OUT_VENDOR)/lib/modules/fpsensor_spi_tee.ko \
+    vendor/motorola/hanoip-modules/lib/modules/modules.alias:$(TARGET_COPY_OUT_VENDOR)/lib/modules/modules.alias \
+    vendor/motorola/hanoip-modules/lib/modules/modules.dep:$(TARGET_COPY_OUT_VENDOR)/lib/modules/modules.dep \
+    vendor/motorola/hanoip-modules/lib/modules/modules.softdep:$(TARGET_COPY_OUT_VENDOR)/lib/modules/modules.softdep 
